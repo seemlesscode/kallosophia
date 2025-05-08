5ea85d4eb85c56e2fd4fd0eda0bfae499e63fda8
Tarek Dinkespiler <tarek.dinkespiler@gmail.com>
2025-05-09 00:20:59 +0200

    ts + apollo server 4

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,30 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "@apollo/server";
+import express from "express";
+import type { Request, Response } from "express";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import http from "http";
+import cors from "cors";
 import { typeDefs } from "./graphql/typeDefs";
 import { resolvers } from "./graphql/resolvers";
 import { PrismaClient } from "@prisma/client";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/use/ws";
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const prisma = new PrismaClient();
+
+const PORT = 4000;
+
+interface ExpressContext {
+  req: Request;
+  res: Response;
+}
 
 const corsOptions = {
   origin: ["http://localhost:3000"],
@@ -10,15 +33,57 @@ const corsOptions = {
   credentials: true,
 };
 
-const prisma = new PrismaClient();
+const ctx = async ({ req, res }: ExpressContext) => ({ req, res, prisma });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: () => ({ prisma }),
-  cors: corsOptions,
-});
+async function startApolloServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
 
-server.listen({ port: 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/graphql",
+  });
+
+  const serverCleanup = useServer({ schema }, wsServer);
+  const server = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
+    introspection: true,
+  });
+
+  await server.start();
+
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(corsOptions),
+    express.json(),
+    expressMiddleware(server, {
+      context: ctx,
+    })
+  );
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen(
+      {
+        host: "0.0.0.0",
+        port: PORT,
+      },
+      resolve
+    )
+  );
+  console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+}
+
+startApolloServer();
