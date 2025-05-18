36ae7e277b22d0fe3e41264533f9830bec1b3461
Tarek Dinkespiler <tarek.dinkespiler@gmail.com>
2025-05-18 22:39:55 +0200

    Add favicon

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -14,8 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head />
+    <html lang="fr">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon.png" />
+      </head>
       <body>
         <ApolloWrapper>{children}</ApolloWrapper>
       </body>
